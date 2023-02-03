f6774824aace4cb32bea32e294f04b9b874d54ce
Vasiko Mtsituri <vasiko.mtsituri@makingscience.com>
2023-02-03 16:46:39 +0400

    add task steps solutions by me(Vaso Mtsituri)

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import pandas as pd
 import numpy as np
 
-URL = "https://sweeftdigitaltalk.slack.com/files/U02UH39GD0C/F04MPJ6GU1H/nyc_tlc_yellow_trips_2018_subset_1.csv"
-df = pd.read_csv(URL)
+csv_file_path = "/home/vaso/Downloads/nyc_tlc_yellow_trips_2018_subset_1.csv"
 
-start_index = 1000
-end_index = 2001
-rows = df[start_index:end_index]
-print(rows)
+df = pd.read_csv(csv_file_path)
+
+length = df.shape[0]
+
+print(length)
+
+print(df['pickup_datetime'])
+
+from datetime import datetime
+print(datetime.utcnow())
+
+# #data obj gardaqmna
+# df["pickup_datetime"] = pd.to_datetime(df['pickup_datetime'].str.split('T')[0] , format = "%Y-%m-%d")
+# df["dropoff_datetime"] = pd.to_datetime(df['dropoff_datetime'].str.split('T')[0] , format = "%Y-%m-%d")
+
+print('Before')
+print(type(df['pickup_datetime'].iloc[0]))
 
 
+df["pickup_datetime"] = df["pickup_datetime"].apply(lambda x: datetime.strptime(x.split('T')[0], '%Y-%m-%d').date())
+df["dropoff_datetime"] = df["dropoff_datetime"].apply(lambda x: datetime.strptime(x.split('T')[0], '%Y-%m-%d').date())
+print('After')
 
-#data obj gardaqmna
-df["pickup_datetime"] = pd.to_datetime(df['pickup_datetime'] , format = "%y%m%d")
-df["dropoff_datetime"] = pd.to_datetime(df['dropoff_datetime'] , format = "%y%m%d")
+print(type(df['pickup_datetime'].iloc[0]))
 
-# 2 velis datoveba
-df = df(['pickup_datetime'])
-df = df(['dropoff_datetime'])
-df.to_csv('new_csvfile', index = False)
+# elis datoveba
+df = df[['pickup_datetime', 'dropoff_datetime']]
 
-#axali velis sheqmna
+print(df.shape)
+
+# #axali velis sheqmna
 df['is_two_or_more_days_ride'] = np.where(df['pickup_datetime'] == df['dropoff_datetime'] , 0 , 1)
-df.to_csv('newer_csvfile' , index = False)
 
-#df.columns = df.columns.str.replace('_', ' ')
+print(df)
